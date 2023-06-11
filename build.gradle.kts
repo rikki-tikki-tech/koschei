aa5a48905a3dcaba2b8de94bfb3e0725092ba417
Egor Utrobin <eg.utrobin@yandex.ru>
2023-06-11 14:28:34 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    id("io.ktor.plugin") version "2.3.1"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.22"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+group = "tech.rikki_tikki"
+version = "0.0.1"
+application {
+    mainClass.set("tech.rikki_tikki.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+ktor {
+    docker {
+        localImageName.set("koschei")
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        portMappings.set(listOf(
+            io.ktor.plugin.features.DockerPortMapping(
+                7001,
+                7001,
+                io.ktor.plugin.features.DockerPortMappingProtocol.TCP
+            )
+        ))
+        externalRegistry.set(
+            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+                appName = provider { "koschei" },
+                username = providers.environmentVariable("DOCKERHUB_USERNAME"),
+                password = providers.environmentVariable("DOCKERHUB_TOKEN")
+            )
+        )
+    }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-resources:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
